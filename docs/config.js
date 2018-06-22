dd1948a2bda37512582d2d98f756ff8bf276b979
Yao <GyonGyon@users.noreply.github.com>
2018-06-22 20:01:54 +0800

    update: split config.js

---
+++ docs/config.js
@@ -0,0 +1,13 @@
+window.$docsify = {
+  name: '',
+  repo: '',
+  loadSidebar: true,
+  autoHeader: true,
+  subMaxLevel: 2,
+  loadNavbar: true,
+  el: '#app',
+//   alias: {
+//     '/.*/_sidebar.md': '/_sidebar.md',
+//     '/.*/__navbar.md_': '/__navbar.md_',
+//   },
+}
